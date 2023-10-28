5103c9714e74d94db60997132d509a7befe22e21
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-28 10:58:21 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,24 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import sotorage
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of API """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """ Retrieves the number of each object by type """
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
