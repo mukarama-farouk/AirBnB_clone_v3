c6f2652f2c199b9df0d8ab089fd4b316d0a8987d
mukarama-farouk <faroukmukarama@gmail.com>
2023-10-28 11:14:13 -0700

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
-from models import sotorage
+from models import storage
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
